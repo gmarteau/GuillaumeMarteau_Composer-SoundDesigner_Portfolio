e20865f62124b7e707e6b951b6da6fdba59b373e
Guillaume Marteau <guillaumemarteau@hotmail.fr>
2021-11-03 16:26:46 +0100

    formulaire de contact OK, ajout FluentSMTP avec sendinblue

---
+++ inc/contact-form-validation.php
@@ -0,0 +1,39 @@
+<?php
+if (isset($_POST['submitted'])) {
+    $has_error = false;
+
+    $email_value = esc_html(trim($_POST['email']));
+    $valid_email = filter_var($email_value, FILTER_VALIDATE_EMAIL);
+    $email_error = '';
+    if ($email_value === '') {
+        $email_error = 'Veuillez renseigner votre adresse email';
+        $has_error = true;
+    } elseif (!$valid_email) {
+        $email_error = 'Le format de l\'adresse email n\'est pas valide';
+        $has_error = true;
+    }
+
+    $object_value = esc_html(trim($_POST['object']));
+    $object_error = '';
+    if ($object_value === '') {
+        $object_error = 'Veuillez renseigner un objet';
+        $has_error = true;
+    }
+
+    $message_value = esc_html(trim($_POST['message']));
+    $message_error = '';
+    if ($message_value === '') {
+        $message_error = 'Veuillez écire un contenu pour votre message';
+        $has_error = true;
+    }
+
+    if (!$has_error) {
+        $email_to = get_option('admin_email');
+        $body = 'From: ' . $email_value . '<br /><br />Message:<br />' . $message_value;
+        $email_status = wp_mail($email_to, $object_value, $body);
+        if ($email_status === true) {
+            unset($email_value, $object_value, $message_value);
+            unset($_POST['email'], $_POST['object'], $_POST['message'], $_POST['submitted']);
+        }
+    }
+}
--- page-contact.php
+++ page-contact.php
@@ -1,13 +1,15 @@
 <?php
+
 /**
  * Template Name: Contact
  */
-
 ?>
+
+<?php require_once('inc/contact-form-validation.php'); ?>
 
 <?php get_header(); ?>
 
-<?php while (have_posts()): the_post() ?>
+<?php while (have_posts()) : the_post() ?>
     <div class="row">
         <h1><?php the_title() ?></h1>
     </div>
@@ -16,20 +18,39 @@
         <?php the_content() ?>
     </div>
 
+    <?php if (isset($email_status)) : ?>
+        <div class="row">
+            <p class="alert alert-<?= $email_status === true ? 'success' : 'danger'; ?>" role="alert">
+                <?= $email_status === true ? 'Votre message a été envoyé avec succès' : 'Une erreur est survenue lors de l\'envoi du message'; ?>
+            </p>
+        </div>
+    <?php endif; ?>
+
     <div class="row">
-        <form action="<?php the_permalink() ?>" methode="post" id="contactForm">
+        <form action="<?php the_permalink() ?>" method="post" id="contactForm" novalidate>
             <div class="mb-3">
                 <label for="emailInput" class="form-label">Votre email</label>
-                <input type="email" name="email" class="form-control" id="emailInput" placeholder="<?php the_field('email_placeholder') ?>" />
+                <input type="email" name="email" class="form-control <?= (isset($email_error) && $email_error !== '') ? 'is-invalid' : ''; ?>" id="emailInput" <?= isset($email_value) ? 'value="' . $email_value . '"' : ''; ?> placeholder="<?php the_field('email_placeholder') ?>" />
+                <?php if ($email_error !== '') : ?>
+                    <p class="invalid-feedback"><?= $email_error ?></p>
+                <?php endif; ?>
             </div>
             <div class="mb-3">
                 <label for="objectInput" class="form-label">Objet</label>
-                <input type="text" name="object" class="form-control" id="objectInput" placeholder="<?php the_field('object_placeholder') ?>" />
+                <input type="text" name="object" class="form-control <?= (isset($object_error) && $object_error !== '') ? 'is-invalid' : ''; ?>" id="objectInput" <?= isset($object_value) ? 'value="' . $object_value . '"' : ''; ?> placeholder="<?php the_field('object_placeholder') ?>" />
+                <?php if ($object_error !== '') : ?>
+                    <p class="invalid-feedback"><?= $object_error ?></p>
+                <?php endif; ?>
             </div>
             <div class="mb-3">
                 <label for="messageInput" class="form-label">Votre message</label>
-                <textarea name="message" id="messageInput" cols="30" rows="10" class="form-control" placeholder="<?php the_field('message_placeholder') ?>"></textarea>
+                <textarea name="message" id="messageInput" cols="30" rows="10" class="form-control <?= (isset($message_error) && $message_error !== '') ? 'is-invalid' : ''; ?>" placeholder="<?php the_field('message_placeholder') ?>"><?= isset($message_value) ? $message_value : ''; ?></textarea>
+                <?php if ($message_error !== '') : ?>
+                    <p class="invalid-feedback"><?= $message_error ?></p>
+                <?php endif; ?>
             </div>
+
+            <input type="hidden" name="submitted" id="submitted" value="true" />
 
             <button type="submit" class="btn btn-primary"><?php the_field('submit_text') ?></button>
         </form>
