d61fa1578d1f6e234222749a041f47b1ab923ccb
Guillaume Marteau <guillaumemarteau@hotmail.fr>
2021-11-22 17:47:29 +0100

    style et contenu front-page OK

--- front-page.php
+++ front-page.php
@@ -1,7 +1,34 @@
 <?php get_header(); ?>
 
 <?php while (have_posts()) : the_post() ?>
-    <h1><?php the_title() ?></h1>
+    <div class="home__title">
+        <p>bonjour, je m'appelle</p>
+        <h1 class="title title--big">Guillaume Marteau</h1>
+    </div>
+
+    <div class="home__services">
+        <p>et je fais</p>
+        <ul class="home__services__list">
+            <li class="service service--1">
+                <h2 class="service__title"><a class="link" href="<?= get_post_type_archive_link('project') . '?skill=composition' ?>">de la musique à l'image</a></h2>
+                <p class="service__description">
+                    Je compose la musique originale de votre projet vidéo, qu'il s'agisse d'un court ou long métrage, d'un spot publicitaire ou d'un jeu-vidéo. Je peux également composer l'identité sonore de votre entreprise.
+                </p>
+            </li>
+            <li class="service service--2">
+                <h2 class="service__title"><a class="link" href="<?= get_post_type_archive_link('project') . '?skill=sound-design' ?>">du sound design</a></h2>
+                <p class="service__description">
+                    Besoin de design sonore sur un projet? Film, jeu-vidéo, installation sonore, identité radiophonique, je vous propose un travail complet afin de répondre au mieux à vos besoins, de la création à la standardisation.
+                </p>
+            </li>
+            <li class="service service--3">
+                <h2 class="service__title"><a class="link" href="<?= get_post_type_archive_link('project') . '?skill=integration' ?>">de l'intégration sonore</a></h2>
+                <p class="service__description">
+                    Vous possédez déjà votre banque de sons et de musiques pour votre jeu-vidéo mais n'avez toujours rien implémenté? Vous recherchez une intégration rapide et efficace sur Unreal Engine ou bien une logique complexe grâce à Wwise? Je suis là pour vous!
+                </p>        
+            </li>        
+        </ul>
+    </div>
 <?php endwhile; ?>
 
 <?php get_footer(); ?>
